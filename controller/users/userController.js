0d6cee5c58ae6ff65e4fbddaf8d5858a3471db99
RRRR <56125991+RHaykal@users.noreply.github.com>
2023-10-28 08:21:43 +0700

    minor change

--- controller/users/userController.js
+++ controller/users/userController.js
@@ -5,17 +5,16 @@ const { genSaltSync, hashSync } = require("bcrypt")
 module.exports = {
     createUser: (req, res) => {
         const body = req.body
-        const salt = genSaltSync(process.env.CRYPT_KEY)
+        const salt = genSaltSync(parseInt(process.env.CRYPT_KEY))
         body.pass = hashSync(body.pass, salt)
         createU(body, (err, results) => {
             if (err) {
-                console.log(err)
                 return res.status(500).json({
-                    success: 0,
-                    message: "Opps, server error :("
+                    message: "Opps, server error :(",
+                    serverMessage: err
                 })
             }
-            return res.status(200).json({
+            return res.status(201).json({
                 success: 1,
                 data: results
             })
@@ -24,14 +23,13 @@ module.exports = {
     getUser: (req, res) => {
         getU((err, results) => {
             if (err) {
-                console.log(err)
                 return res.status(500).json({
-                    success: 0,
-                    message: "Opps, server error :("
+                    message: "Opps, server error 😢",
+                    serverMessage: err
                 })
             }
             return res.status(200).json({
-                success: 1,
+                message: "GET Success",
                 data: results
             })
         })
@@ -40,46 +38,51 @@ module.exports = {
         const id = req.params.id
         getUID(id, (err, results) => {
             if (err) {
-                console.log(err)
                 return res.status(500).json({
-                    success: 0,
-                    message: "Opps, server error :("
+                    message: "Opps, server error :(",
+                    serverMessage: err
                 })
             } else if (!results) {
-                return res.json({
-                    message: "no record found boss"
+                return res.status(404).json({
+                    message: "no user data can be found boss"
                 })
             } else {
                 return res.status(200).json({
-                    success: 1,
+                    message: "GET user by id success",
                     data: results
                 })
             }
         })
     },
     updateUser: (req,res) => {
-        const id = req.params.id
-        const body = req.body
-        const salt = genSaltSync(10)
-        body.pass = hashSync(body.pass, salt)
-        updateU(id, body, (err, results) => {
-            if (err) {
-                console.log(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Opps, server error :("
-                })
-            } else if (!results) {
-                return res.json({
-                    message: "failed to update"
-                })
-            } else {
-                return res.status(200).json({
-                    success: 1,
-                    data: results
-                })
-            }
-        })
+        if(Object.keys(req.body).length === 0) {
+            res.status(400).json({
+                message: "Opps, please fill out the form",
+            })
+        } else {
+            const id = req.params.id
+            const body = req.body
+            const salt = genSaltSync(parseInt(process.env.CRYPT_KEY))
+            body.pass = hashSync(body.pass, salt)
+            updateU(id, body, (err, results) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Opps, server error :(",
+                        serverMessage: err
+                    })
+                } else if (!results) {
+                    return res.json({
+                        message: "failed to update"
+                    })
+                } else {
+                    return res.status(201).json({
+                        success: 1,
+                        results: results,
+                        data: body
+                    })
+                }
+            })
+        }
     },
     deleteUser: (req, res) => {
         const id = req.params.id
